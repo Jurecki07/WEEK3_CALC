d21e93339c54e9d44e05a430a7241383040259fa
Tyler Jurecki <jurecki07@yahoo.com>
2017-08-04 11:01:01 -0500

    HTML GOOD

--- Main.js
+++ Main.js
@@ -1,14 +1,11 @@
 // Get all the keys from document
-var button = document.getElementsByClassName('.button');
-var buttonfunction = document.getElementsByClassName( '.buttonfunction');
-var screen = document.getElementsByClassName('.screen')
-
+var button = document.querySelectorAll('.button');
+var screen = document.querySelector('.screen')
 
 // Add onclick event to all the keys and perform operations
-for (var i = 0; i < button.length; i++) {
+for (var i = 0; i < button; i++) {
     button[i].onclick = function () {
         // Get the input and button values
-        var input = document.getElementsByClassName('.screen');
         var inputVal = input.innerHTML;
         var btnVal = this.innerHTML;
     }
